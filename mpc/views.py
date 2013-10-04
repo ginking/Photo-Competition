3406ec5535751d8fddcf9146f981415f93260665
Veneta <veneta@veneta-pc102.(none)>
2013-10-04 15:52:54 +0300

    Update index.html and base.html

--- mpc/views.py
+++ mpc/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def index(request):
-    return HttpResponse("Melon Photo Challenge")
+    return render_to_response('index.html', {})
+#     return HttpResponse("Melon Photo Challenge")
